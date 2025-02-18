5c3c9c7d63340e77bdab4de6d6dcf894814d80f2
Butseraentjes <peterbutseraen@gmail.com>
2025-02-18 14:43:06 +0100

    Create config.js

---
+++ js/lib/config.js
@@ -0,0 +1,15 @@
+// config.js
+export const config = {
+    firebase: {
+        apiKey: "AIzaSyAgT_uX_5RrP7CKiI5-KpBSUXvvT928qik",
+        authDomain: "wandeltracker-3692d.firebaseapp.com",
+        projectId: "wandeltracker-3692d",
+        storageBucket: "wandeltracker-3692d.appspot.com",
+        messagingSenderId: "131040578819",
+        appId: "1:131040578819:web:1aabbd1272a76fdc232d36"
+    },
+    graphhopper: {
+        apiKey: "1fb0ae3f-1d34-4c16-896d-02ee3f40d4ec",
+        baseUrl: "https://graphhopper.com/api/1"
+    }
+};
